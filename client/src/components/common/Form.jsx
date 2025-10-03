e89126edc9cc72afa6b31d81606931134d755013
Nilesh020901 <nileshsoni0209@gmail.com>
2025-10-03 16:16:20 +0530

    working on form

--- client/src/components/common/Form.jsx
+++ client/src/components/common/Form.jsx
@@ -1,8 +1,70 @@
+import { Select, SelectTrigger, SelectValue } from "@radix-ui/react-select";
 
-function CommonForm() {
+function CommonForm({ formFields, formData, setFormData, onSubmit, buttonText }) {
+
+    function renderInputField(field) {
+        let element = null;
+        switch (field.type) {
+            case 'input':
+                element = <input
+                    type={field.type}
+                    name={field.name}
+                    placeholder={field.placeholder}
+                    id={field.id}
+                />;
+                break;
+
+            case 'select':
+                element = (
+                    <Select>
+                        <SelectTrigger className="w-full">
+                            <SelectValue placeholder={field.placeholder} />
+                        </SelectTrigger>
+                        <SelectContent>
+                            {
+                                field.options && field?.options.length > 0 ?
+                                    field.options.map(option => <SelectItem key={option.id} value={option.id}>{option.label}</SelectItem>)
+                                    : null
+                            }
+                        </SelectContent>
+                    </Select>
+                )
+                break;
+
+            case 'textarea':
+                element = (
+                    <textarea
+                        placeholder={field.placeholder}
+                        name={field.name}
+                        id={field.id}
+                    />
+                );
+                break;
+
+            default:
+                element = <input
+                    type={field.type}
+                    name={field.name}
+                    placeholder={field.placeholder}
+                    id={field.id}
+                />;
+                break;
+        }
+
+        return element;
+    }
     return (
         <form>
-
+            <div className="flex flex-col gap-3">
+                {
+                    formFields.map(field => <div key={field.id} className="grid w-full gap-1.5">
+                        <label htmlFor={field.name} className="mb-1">{field.label}</label>
+                        {
+                            renderInputField(field)
+                        }
+                    </div>)
+                }
+            </div>
         </form>
     )
 }
--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,20 +1,23 @@
 export const registerFormControls = [
     {
+        id: 1,
         name : 'userName',
         label : 'User Name',
         placeholder : 'Enter your user name',
         type : 'text',
     },
     {
+        id: 2,
         name : 'email',
         label : 'Email',
         placeholder : 'Enter your email!',
         type : 'text',
     },
     {
+        id: 3,
         name : 'password',
         label : 'password',
         placeholder : 'Enter password',
-        type : 'text',
+        type : 'password',
     }
 ]
